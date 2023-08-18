8ca35ea970e3f0855e37d6cc38c81d0a66db956e
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-18 18:14:34 +0530

    Create DeleteNode-2.py
    method 2

---
+++ Data Structure/2.Linked list/DeleteNode-2.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+    def delete_node(self, value):
+        if self.head is None:
+            return
+
+        if self.head.data == value:
+            self.head = self.head.next
+            return
+
+        current = self.head
+        while current.next:
+            if current.next.data == value:
+                current.next = current.next.next
+                return
+            current = current.next
+
+
+# Creating a linked list
+my_linked_list = LinkedList()
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+my_linked_list.append(6)
+my_linked_list.append(7)
+
+print("Original linked list:")
+my_linked_list.display()
+
+# Deleting a node with value 2
+value_to_delete = 6
+my_linked_list.delete_node(value_to_delete)
+
+print("\nLinked list after deletion:")
+my_linked_list.display()
