dc6b97dc868c20f24e18a880254f10ffc7d40276
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-26 15:55:58 +0530

    Create push-pop-ele-1.py

---
+++ Data Structure/3.Stack programs/push-pop-ele-1.py
@@ -0,0 +1,19 @@
+# A Python program that creates a stack of elements and then pushes and pops elements from the stack.
+def push(stack, element):
+  stack.append(element)
+
+def pop(stack):
+  return stack.pop()
+
+def main():
+  stack = []
+
+  push(stack, 1)
+  push(stack, 2)
+  push(stack, 3)
+
+  print("The top element of the stack is:", pop(stack))
+  print("The stack now contains:", stack)
+
+if __name__ == "__main__":
+  main()
