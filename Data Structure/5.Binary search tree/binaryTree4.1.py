77cf62abb8592f35fa185a7fcab9d7052ff9b146
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-26 17:16:04 +0530

    add code

---
+++ Data Structure/5.Binary search tree/binaryTree4.1.py
@@ -0,0 +1,40 @@
+# A python program that finds the minimum and maximum elements in a binary search tree.
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def minValueNode(node):
+    current = node
+    while current.left is not None:
+        current = current.left
+    return current
+
+def maxValueNode(node):
+    current = node
+    while current.right is not None:
+        current = current.right
+    return current
+
+def findMinMax(root):
+    if root is None:
+        return (None, None)
+
+    minNode = minValueNode(root)
+    maxNode = maxValueNode(root)
+
+    return (minNode.data, maxNode.data)
+
+# Example usage
+if __name__ == "__main__":
+    root = Node(15)
+    root.left = Node(10)
+    root.right = Node(20)
+    root.left.left = Node(8)
+    root.left.right = Node(12)
+    root.right.right = Node(25)
+
+    (minValue, maxValue) = findMinMax(root)
+    print("The minimum value in the tree is", minValue)
+    print("The maximum value in the tree is", maxValue)
---
+++ Data Structure/5.Binary search tree/binaryTree4.2.py
@@ -0,0 +1,40 @@
+# A python program that finds the minimum and maximum elements in a binary search tree.
+class TreeNode:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def find_min(root):
+    if root is None:
+        return None
+
+    current = root
+    while current.left is not None:
+        current = current.left
+    return current.val
+
+def find_max(root):
+    if root is None:
+        return None
+
+    current = root
+    while current.right is not None:
+        current = current.right
+    return current.val
+
+# Example usage
+if __name__ == "__main__":
+    root = TreeNode(15)
+    root.left = TreeNode(10)
+    root.right = TreeNode(20)
+    root.left.left = TreeNode(8)
+    root.left.right = TreeNode(12)
+    root.right.left = TreeNode(17)
+    root.right.right = TreeNode(25)
+
+    min_value = find_min(root)
+    max_value = find_max(root)
+
+    print("Minimum value:", min_value)
+    print("Maximum value:", max_value)
