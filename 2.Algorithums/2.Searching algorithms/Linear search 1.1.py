ab42da4cfd172108b4d837e373ca32b84ae9338d
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-26 18:53:07 +0530

    Update Linear search 1.1.py
    code update

--- 2.Algorithums/2.Searching algorithms/Linear search 1.1.py
+++ 2.Algorithums/2.Searching algorithms/Linear search 1.1.py
@@ -1,3 +1,30 @@
-# Linear search: A searching algorithm that compares each element of the array with 
+# Linear search: A searching algorithm that compares each element of the array with
 # the target value and returns the index of the first occurrence of the target value.
 # Method 1
+def linear_search(array, target):
+    index = 0
+
+    while index < len(array):
+        if array[index] == target:
+            return index
+
+        index += 1
+
+    return -1
+
+
+def main():
+    array = [10, 2, 5, 3, 1, 9, 8, 7, 6, 4]
+
+    target = 8
+
+    index = linear_search(array, target)
+
+    if index != -1:
+        print("The target value is at index", index)
+    else:
+        print("The target value is not found")
+
+
+if __name__ == "__main__":
+    main()
