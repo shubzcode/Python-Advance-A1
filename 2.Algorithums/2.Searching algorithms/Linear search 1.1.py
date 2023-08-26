4e1954db43ab92a87ce624baa05bd0cdf8a53024
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-26 18:52:03 +0530

    files created
    files created

---
+++ 2.Algorithums/2.Searching algorithms/Linear search 1.1.py
@@ -0,0 +1,3 @@
+# Linear search: A searching algorithm that compares each element of the array with 
+# the target value and returns the index of the first occurrence of the target value.
+# Method 1
