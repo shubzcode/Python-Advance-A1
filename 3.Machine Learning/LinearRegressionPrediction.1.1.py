9af7d1aa970f6d6e4c4de51fff3c1b7525da7e77
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-28 22:50:13 +0530

    update

---
+++ 3.Machine Learning/LinearRegressionPrediction.1.1.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# Generate some data
+x = np.linspace(0, 10, 100)
+y = 2*x + 5
+
+# Create a linear regression model
+model = LinearRegression()
+model.fit(x.reshape(-1, 1), y)
+
+# Predict the output for a new input
+x_new = 12
+y_pred = model.predict(x_new.reshape(-1, 1))
+
+# Plot the data and the model
+plt.plot(x, y, label="Data")
+plt.plot(x_new, y_pred, label="Prediction")
+plt.legend()
+plt.show()
